d6302e32ea82ca1cbe69d94be3e2f49ac1125bda
vivesing <vivek.singh1693@gmail.com>
2024-11-11 19:58:15 +0530

    [spring]Adding the base project

---
+++ sample-springsecurity-app/src/main/java/com/viv/SampleSpringsecurityAppApplication.java
@@ -0,0 +1,13 @@
+package com.viv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringsecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringsecurityAppApplication.class, args);
+	}
+
+}
