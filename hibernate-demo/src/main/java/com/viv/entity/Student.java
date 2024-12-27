d536eeb5ebcf15add94a5e8c3b027253317b6b68
vivesing <vivek.singh1693@gmail.com>
2024-12-27 11:49:30 +0530

    [spring_jpa]Adding changes for list

--- hibernate-demo/src/main/java/com/viv/entity/Student.java
+++ hibernate-demo/src/main/java/com/viv/entity/Student.java
@@ -1,6 +1,8 @@
 package com.viv.entity;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 
@@ -12,6 +14,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OrderColumn;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -31,7 +34,13 @@ public class Student {
     @Column(name = "last_name")
     private String lastName;
 
-    private String email;
+
+    @ElementCollection
+    @CollectionTable(name = "emails", //default student_emails
+    joinColumns = @JoinColumn(name = "student_id")) 
+    @OrderColumn //default column email_order
+    @Column(name = "email") 
+    private List<String> email = new ArrayList<>();
 
 
     @ElementCollection
--- hibernate-demo/src/main/resources/sql/scripts.sql
+++ hibernate-demo/src/main/resources/sql/scripts.sql
@@ -7,3 +7,10 @@ create table image(
 student_id int(11) not null,
 file_name varchar(45) default null
 )
+
+
+create table emails(
+student_id int(11) not null,
+email_order int(11) not null, 
+email varchar(45) default null
+)
